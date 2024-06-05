82ff42179584696e0bbd1f9d693bccf8f3b32c83
unknown <muravskijandrej6@gmail.com>
2024-06-05 21:46:50 +0100

    first

---
+++ studyforit/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    data = {
+       'title': 'Hel my new ',
+       'values': ['some','ht;o','123443']
+    }
+    return render(request, 'main/index.html', data)
+def about(request):
+    return render(request, 'main/about.html')
---
+++ studyforit/news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Naming', max_length=50 )
+    anons = models.CharField('Anounce', max_length=250 )
+    full_text = models.TextField('textforanoun' )
+    date = models.DateTimeField('Date of Publication')
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
+    class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "Newses"
